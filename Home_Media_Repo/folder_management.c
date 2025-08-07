eccdcc469851450db9f8bc230274482b45ec729a
apoda008 <161513572+apoda008@users.noreply.github.com>
2025-08-06 17:35:12 -0700

    Getting ffmpeg to transcode files and move them

--- Home_Media_Repo/folder_management.c
+++ Home_Media_Repo/folder_management.c
@@ -1,6 +1,112 @@
 #include "folder_management.h"
 #include "networking.h"
 
+int CallSystemWithTCHAR(const TCHAR* cmd) {
+#ifdef UNICODE
+    // Convert TCHAR (wchar_t*) to char*
+    char narrow_cmd[512];
+    WideCharToMultiByte(CP_ACP, 0, cmd, -1, narrow_cmd, sizeof(narrow_cmd), NULL, NULL);
+    return system(narrow_cmd);
+#else
+    return system(cmd); // TCHAR is char, so this is fine
+#endif
+}
+
+int Convert_to_Mp4(TCHAR* filename_path, TCHAR* title, Master_Directory* global_ptr) {
+	//need to remove the file extension from the title
+	//This will be used to transcode the video into mp4 format
+	int strlen_title = _tcslen(title);
+	
+	title[strlen_title - 4] = '\0'; //removes the last 4 characters (assumes .mov, .avi, etc.)
+
+    TCHAR* cmd[1024];
+
+    _snwprintf_s(
+        cmd,                 
+        1024,                
+        _TRUNCATE,          
+        L"ffmpeg -i \"%s\" -c:v libx264 -c:a aac -movflags +faststart \"%s\\%s.mp4\"",
+        filename_path,
+        global_ptr->path_to_media,
+        title
+    );
+
+    _tprintf(_T("\nCOMMAND: %s\n"), cmd);
+    
+    int result = CallSystemWithTCHAR(cmd);
+
+    if (result != 0)
+    {
+        printf("\nVideo transcoding failed\n");
+        return 0;
+    }
+    else {
+        printf("\nVideo transcoding successful\n");
+        TCHAR* output_video = _T("output_video.mp4");
+        return output_video; //return the path to the transcoded video
+    }
+
+    return 1;
+}
+
+void Copy_To_Mp4(TCHAR* path, Master_Directory* global_ptr) {
+    TCHAR copy_path[MAX_PATH];
+
+    //sets the * for a search through all files in the directory
+    _tcscpy_s(copy_path, MAX_PATH, path);
+    _tcscat_s(copy_path, MAX_PATH, _T("\\*"));
+
+    WIN32_FIND_DATA findFileData;
+    HANDLE hFind = FindFirstFile(copy_path, &findFileData);
+
+    if (hFind == INVALID_HANDLE_VALUE) {
+        _tprintf(_T("Error handle value. Error: %lu \n"), GetLastError());
+    }
+    else {
+        do {
+
+            //Skip "." ".." file names
+            if ((strcmp(findFileData.cFileName, ".") == 0) || (strcmp(findFileData.cFileName, "..") == 0)) {
+                continue;
+            }
+
+            TCHAR next_dir[MAX_PATH];
+
+            _tcscpy_s(next_dir, MAX_PATH, path);
+            _tcscat_s(next_dir, MAX_PATH, _T("\\"));
+            _tcscat_s(next_dir, MAX_PATH, findFileData.cFileName);
+            //_tprintf(_T("NEXT_DIR: %s\n"), next_dir);
+
+            //check if it's a directory or a file
+            if (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+                //_tprintf(_T("[DIR]: %s\n"), findFileData.cFileName);
+
+				Copy_To_Mp4(next_dir, global_ptr); //recursive call for subdirectories
+
+            }
+            else {
+                //_tprintf(_T("[FILE]: %s\n"), findFileData.cFileName);
+                //This creates a directory path with the target filename
+                TCHAR dest[MAX_PATH];
+                _tcscpy_s(dest, MAX_PATH, path);
+                _tcscat_s(dest, MAX_PATH, _T("\\"));
+                _tcscat_s(dest, MAX_PATH, findFileData.cFileName);
+
+				//Converts the file to mp4 for stream and moves it to the media folder
+				Convert_to_Mp4(dest, findFileData.cFileName, global_ptr);
+
+            }
+
+        } while (FindNextFile(hFind, &findFileData) != 0);
+
+        if (GetLastError() != ERROR_NO_MORE_FILES) {
+            printf("FindNextFile Failed (%d)\n", GetLastError());
+        }
+
+        FindClose(hFind);
+    }
+
+}
 
 bool BrowseForFolder(Master_Directory* ptr, int create_or_move) {
     
@@ -339,7 +445,14 @@ void FolderExecution(Master_Directory* global_ptr) {
     TCHAR copy[MAX_PATH];
 
     _tcscpy_s(copy, MAX_PATH, global_ptr->path_to_media_for_import);
-    MoveMediaToMaster(copy, global_ptr);
+	
+    //Media will not be moved. It will be copied to the master folder
+	//once implemented this will be removed entirely
+    //MoveMediaToMaster(copy, global_ptr);
+	printf("Moving media to master folder...\n");
+	printf("WARNING: This will take some time.\n");
+
+	Copy_To_Mp4(global_ptr->path_to_media_for_import, global_ptr);
     
     //some point needs to ask user to do this
     File_Search_Parse(global_ptr);
--- Home_Media_Repo/main.c
+++ Home_Media_Repo/main.c
@@ -10,6 +10,8 @@ int main() {
     static struct Master_Directory master_pathing;
     static struct Master_Directory* dir_struct_ptr = &master_pathing;
     master_pathing.tmdb_limiter = 0;
+	master_pathing.num_of_files = 0;
+	master_pathing.size_of_hash = 0; 
 
     //printf("Select a location to save the Repository.\n");
     //HRESULT hr = CoInitialize(NULL);
@@ -49,48 +51,13 @@ int main() {
     //DB waits for commands 
     //..
 
-    //TEST REGION
-
+    //para needs to be num of files 
 	DatabaseStructure* test = Construct_Database_Structure(10, 10);
-
-    /*
-	printf("Movie Set Size: %zu\n", test->movie_set_size);
-	printf("Series Set Size: %zu\n", test->series_set_size);
-	printf("Movie elements: %d\n", test->movies->num_elements_MV);
-	printf("Series elements: %d\n", test->series->num_elements_Se);
-    printf("\n");
-
-    Insert_Movie(test, "fired up", "this is a description", _T("C://main//thing"), 4050);
-    printf("Movie elements: %d\n", test->movies->num_elements_MV);
-    Insert_Movie(test, "Not fired up", "this is a description2", _T("C://main//thing2"), 4050);
-    printf("Movie elements: %d\n", test->movies->num_elements_MV);
-    Insert_Movie(test, "fired up 2", "this is a description3", _T("C://main//thing3"), 4050);
-    printf("Movie elements: %d\n", test->movies->num_elements_MV);
-	printf("\n");
-
-	printf("Movie ID: %d\n", test->movies->id[0]);
-	printf("Movie Title: %s\n", test->movies->title[0]);
-	printf("Movie Description: %s\n", test->movies->description[0]);
-	printf("Movie Dir Position: %s\n", test->movies->dir_position[0]);
-	printf("Movie Video Size: %d\n", test->movies->video_size[0]);
-
-	printf("Movie ID: %d\n", test->movies->id[1]);
-	printf("Movie Title: %s\n", test->movies->title[1]);
-	printf("Movie Description: %s\n", test->movies->description[1]);
-	printf("Movie Dir Position: %s\n", test->movies->dir_position[1]);
-	printf("Movie Video Size: %d\n", test->movies->video_size[1]);
-
-	printf("Movie ID: %d\n", test->movies->id[2]);
-	printf("Movie Title: %s\n", test->movies->title[2]);
-	printf("Movie Description: %s\n", test->movies->description[2]);
-	printf("Movie Dir Position: %s\n", test->movies->dir_position[2]);
-	printf("Movie Video Size: %d\n", test->movies->video_size[2]);
-    */
 
     Fill_Table_Movies(test, dir_struct_ptr);
     
     Print_Movie_Table(test->movies);
-    //END TEST REGION
+    
 
 
 
